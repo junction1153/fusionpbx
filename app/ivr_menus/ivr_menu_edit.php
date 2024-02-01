3f9c8eb6b33db0f842f3d09a45e34ed3f0f96ff1
Joseph <71848841+junction1153@users.noreply.github.com>
2024-02-01 11:55:36 -0500

    IVR CID prefix

--- app/ivr_menus/ivr_menu_edit.php
+++ app/ivr_menus/ivr_menu_edit.php
@@ -364,7 +364,8 @@
 					}
 					else {
 						if (!empty($ivr_menu_cid_prefix)) {
-							$dialplan_xml .= "		<action application=\"set\" data=\"caller_id_name=".xml::sanitize($ivr_menu_cid_prefix)."#\${caller_id_name}\"/>\n";
+//							$dialplan_xml .= "		<action application=\"set\" data=\"caller_id_name=".xml::sanitize($ivr_menu_cid_prefix)."#\${caller_id_name}\"/>\n";
+							$dialplan_xml .= "		<action application=\"set\" data=\"caller_id_name=".xml::sanitize($ivr_menu_cid_prefix)."#\${caller_id_name}\"/>\n"; //JA
 							$dialplan_xml .= "		<action application=\"set\" data=\"effective_caller_id_name=\${caller_id_name}\"/>\n";
 						}
 						$dialplan_xml .= "		<action application=\"ivr\" data=\"".xml::sanitize($ivr_menu_uuid)."\"/>\n";
